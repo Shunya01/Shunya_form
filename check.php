f9d58275616da40317a7ab34ce7964216db80cc9
渡邉舜也 <shun.1630.effort@gmail.com>
2019-06-01 22:53:28 +0900

    bootstrapを用いて装飾を行いました。
    index.html, check.html, thanks.phpに色をつけ、グリッドシステムを追加した。

--- check.php
+++ check.php
@@ -15,48 +15,77 @@ $content=h($_POST['content']);
 // echo $nickname;
 
 if ($nickname == '') {
-        $nickname_result = 'ニックネームが入力されてません。';
-    } else {
-        $nickname_result = 'ようこそ、' . $nickname . '様';
-    }
+    $nickname_result = 'ニックネームが入力されてません。';
+} else {
+    $nickname_result = 'ニックネーム: ' . $nickname . ' 様';
+}
 
-    if ($email == '') {
-        $email_result = 'メールアドレスが入力されてません。';
-    } else {
-        $email_result = 'メールアドレス:' . $email;
-    }
+if ($email == '') {
+    $email_result = 'メールアドレスが入力されてません。';
+} else {
+    $email_result = 'メールアドレス: ' . $email;
+}
 
-    if ($content == '') {
-        $content_result = 'お問い合わせ内容が入力されてません。';
-    } else {
-        $content_result = 'お問い合わせ内容:' . $content;
-    }
+if ($content == '') {
+    $content_result = 'お問い合わせ内容が入力されてません。';
+} else {
+    $content_result = 'お問い合わせ内容: ' . $content;
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>入力内容確認</title>
-    </head>
-    <body>
-        <h1>入力内容確認</h1>
-        <p><?php echo $nickname_result ?></p>
-        <p><?php echo $email_result ?></p>
-        <p><?php echo $content_result ?></p>
+<head>
+    <meta charset="utf-8">
+    <title>入力内容確認</title>
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
 
-        <form method="POST" action="thanks.php">
-            <input type="hidden" name="nickname" value="<?php echo $nickname?>">
-            <input type="hidden" name="email" value="<?php echo $email?>">
-            <input type="hidden" name="content" value="<?php echo $content?>">
+    <div class="container">
 
-            <input type="button" value="戻る" onclick="history.back()">
-            <?php if ($email !='' && $nickname != '' && $content!=''):
+        <div class="row">
+          <h1 class="jumbotron text-center bg-info col-12">お問い合わせ内容を確認してください</h1>
+      </div>
+
+      <div class="card">
+        <div class="row">
+          <div class="card-header bg-success col-12">
+            <h3>入力内容確認画面</h3>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="card-body bg-warning">
+            <blockquote class="blockquote mb-0">
+              <p><?php echo $nickname_result ?></p>
+              <p><?php echo $email_result ?></p>
+              <p><?php echo $content_result ?></p>
+          </blockquote>
+      </div>
+  </div>
+
+</div>
+
+<form method="POST" action="thanks.php">
+    <input type="hidden" name="nickname" value="<?php echo $nickname?>">
+    <input type="hidden" name="email" value="<?php echo $email?>">
+    <input type="hidden" name="content" value="<?php echo $content?>">
+
+    <div class="row">
+        <input type="button" value="戻る" onclick="history.back()" class="btn-primary btn-lg ml-5 mt-3 center-block col-2">
+        <?php if ($email !='' && $nickname != '' && $content!=''):
             ///コロン構文    ?>
-            <input type="submit" value="OK">
-           <?php endif;?> 
-        </form>
-    </body>
+
+            <input type="submit" value="送信" class="btn-primary btn-lg ml-5 mt-3 col-2">
+        <?php endif;?> 
+    </div>
+
+</form>
+
+</div>
+
+</body>
 
 </html>
--- thanks.php
+++ thanks.php
@@ -20,16 +20,44 @@ $stmt->execute([$nickname,$email,$content]);
 
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>送信完了</title>
-    </head>
-    <body>
-        <h1>お問い合わせありがとうございました！</h1>
-        <p><?php echo $nickname ?></p>
-        <p><?php echo $email ?></p>
-        <p><?php echo $content ?></p>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>送信完了</title>
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="row">
+            <h1 class="jumbotron text-center bg-info col-12">お問い合わせありがとうございました！</h1>
+        </div>
+        <div class="card">
+            <div class="row">
+              <div class="card-header bg-success col-12">
+                <h3>以下の内容でお問い合わせを受け付けました。</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="card-body bg-warning col-12">
+                <blockquote class="blockquote ">
+                  <p><?php echo "ニックネーム：".$nickname ?></p>
+                  <p><?php echo "メールアドレス:".$email ?></p>
+                  <p><?php echo "お問い合わせ：".$content ?></p>
+              </blockquote>
+          </div>
+      </div>
+
+  </div>
+
+  <div class="row">
+    <a href="index.html" class="mt-4">お問い合わせ受け付けページに戻る</a>
+</div>
+
+
+</div>
+</body>
 
 
 
