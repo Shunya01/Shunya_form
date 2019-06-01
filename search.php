32e1da9a6b248396a0534c48db25c2939021dd6d
渡邉舜也 <shun.1630.effort@gmail.com>
2019-06-02 00:34:23 +0900

    ニックネームのみならず全てのカラムにおける検索を可能にした。装飾。
    検索で全てのカラムにおける検索が出来るようになった。

--- search.php
+++ search.php
@@ -5,12 +5,12 @@ require_once ('function.php');
 require_once ('dbconnect.php');
 
 $nickname = '';
-if (isset($_GET['nickname'])) {
-    $nickname = $_GET['nickname'];
+if (isset($_GET['searchWord'])) {
+    $searchWord = $_GET['searchWord'];
 }
 
-$stmt = $dbh->prepare('SELECT * FROM surveys WHERE nickname like ?');
-$stmt->execute(["%$nickname%"]);
+$stmt = $dbh->prepare('SELECT * FROM `surveys` WHERE CONCAT(nickname, email,content) LIKE ?');
+$stmt->execute(["%$searchWord%"]);
 $results = $stmt->fetchAll();
 ?>
 
@@ -18,18 +18,30 @@ $results = $stmt->fetchAll();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>検索システム</title>
+    <link rel="stylesheet" href="bootstrap.css">
 </head>
 <body>
-    <form action="search.php" method="GET">
-        <p>検索したいnicknameを入力してください。</p>
-        <input type="text" name="nickname">
+    <div class="container">
+
+    <div class="row">
+    <h1 class="jumbotron text-center bg-info col-12">データベース検索システム</h1>
+    </div>
+
+    <div class="row">
+    <form action="search.php" method="GET" class="mb-5">
+        <p>キーワードを入力すると、キーワードを含むデータを検索することができます。</p>
+        <input type="text" name="searchWord">
         <input type="submit" value="検索">
     </form>
+    </div>
+
     <?php foreach ($results as $result) : ?>
         <p><?php echo $result['nickname']?></p>
         <p><?php echo $result['email']?></p>
         <p><?php echo $result['content']?></p>
+        <hr style="border:0;border-top:1px solid blue;">
     <?php endforeach; ?>
+　　</div>  
 </body>
 </html>
